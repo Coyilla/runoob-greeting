27b7574a6b491c3d18994e26b65e7f60518199b1
Coyilla <1148195806@qq.com>
2025-09-20 09:44:41 +0800

    添加数据库及数据库数据

---
+++ db/db.sql
@@ -0,0 +1,13 @@
+DROP TABLE if EXISTS course;
+CREATE TABLE course(  
+    id SERIAL PRIMARY KEY,
+    teacher_id INT NOT NULL,
+    name VARCHAR(140) NOT NULL,
+    time TIMESTAMP DEFAULT now()
+);
+
+INSERT INTO course(id,teacher_id,name,time)
+VALUES(1,1,'First course','2022-01-17 05:40:00');
+
+INSERT INTO course(id,teacher_id,name,time)
+VALUES(2,1,'Second course','2022-01-18 05:45:00');
