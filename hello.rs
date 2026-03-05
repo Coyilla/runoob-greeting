7d2ed26416914bf57c9a7a81f401a44898c4511a
Coyilla <1148195806@qq.com>
2026-03-05 21:46:11 +0800

    一些更改

---
+++ hello.rs
@@ -0,0 +1,7 @@
+fn main(){
+    println!("Hello World!");
+}
+
+// 生成可执行文件
+//$ rustc hello.rs
+
--- ws/webservice/src/handlers.rs
+++ ws/webservice/src/handlers.rs
@@ -88,19 +88,25 @@ mod tests{
     use super::*;
     use actix_web::http::StatusCode;
     use std::sync::Mutex;
+    use dotenv::dotenv;
+    use sqlx::postgres::PgPoolOptions;
+    use std::env;
 
     #[actix_rt::test]
     async fn  post_course_test() {
+        dotenv().ok();
+        let db_url=env::var("DATABSE_URL is not set.");
+        let db_pool=PgPoolOptions::new().connect(&db_url).await.unwrap();
+        let app_state:web::Data<AppState>=web::Data::new(AppState { 
+            health_check_response:"".to_string(),
+            visit_count: Mutex::new(0), 
+            db: db_pool,
+        });
         let course = web::Json(Course{
             teacher_id:1,
             name:"Test course".into(),
             id:None,
             time:None,
-        });
-        let app_state:web::Data<AppState>=web::Data::new(AppState { 
-            health_check_response: "".to_string(),
-            visit_count: Mutex::new(0), 
-            courses: Mutex::new(vec![]),
         });
         let resp = new_course(course, app_state).await;
         assert_eq!(resp.status(),StatusCode::OK);
@@ -108,10 +114,17 @@ mod tests{
 
     #[actix_rt::test]
     async fn get_all_courses_seccess(){
-        let app_state: web::Data<AppState>=web::Data::new(AppState { 
-            health_check_response: "".to_string(),
-            visit_count: Mutex::new(0),
-            courses: Mutex::new(vec![])
+        // let app_state: web::Data<AppState>=web::Data::new(AppState { 
+        //     health_check_response: "".to_string(),
+        //     visit_count: Mutex::new(0),
+        //     db:db_pool,
+        // });
+        let db_url=env::var("DATABSE_URL is not set.");
+        let db_pool=PgPoolOptions::new().connect(&db_url).await.unwrap();
+        let app_state:web::Data<AppState>=web::Data::new(AppState { 
+            health_check_response:"".to_string(),
+            visit_count: Mutex::new(0), 
+            db: db_pool,
         });
         let teacher_id :web::Path<(usize,)> =web::Path::from((1,));
         let resp =get_courses_for_teacher(app_state, teacher_id).await ;
